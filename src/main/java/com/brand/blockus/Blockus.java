c17ded4e1d95179f6f4af8a4ff69144f3a6cea5f
Brandcraf06 <Brandcraft06@gmail.com>
2019-01-30 21:07:37 +0100

    19w05a

--- src/main/java/com/brand/blockus/Blockus.java
+++ src/main/java/com/brand/blockus/Blockus.java
@@ -11,7 +11,7 @@ import net.minecraft.item.Items;
 public class Blockus implements ModInitializer {
 	
 	public static final String MOD_ID = "blockus";
-	public static final String VERSION = "1.0.4";
+	public static final String VERSION = "1.0.5";
 	public static final String NAME = "Blockus";
 	
 	@Override
