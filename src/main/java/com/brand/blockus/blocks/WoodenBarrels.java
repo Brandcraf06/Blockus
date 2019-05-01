04c160c7e211bca9b120269385f30ded72b1c18d
Yanis48 <doublecraft.official@gmail.com>
2019-05-01 23:22:33 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/main/java/com/brand/blockus/blocks/WoodenBarrels.java
+++ src/main/java/com/brand/blockus/blocks/WoodenBarrels.java
@@ -1,17 +1,22 @@
 package com.brand.blockus.blocks;
 
+import java.util.Random;
+
 import com.brand.blockus.Blockus;
 import com.brand.blockus.blocks.entity.WoodenBarrelsEntity;
 
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.minecraft.block.BarrelBlock;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class WoodenBarrels extends BarrelBlock {
 
@@ -22,7 +27,15 @@ public class WoodenBarrels extends BarrelBlock {
     }
 	
 	@Override
-	public BlockEntity createBlockEntity(BlockView blockView_1) {
+	public void onScheduledTick(BlockState state, World world, BlockPos pos, Random random) {
+		BlockEntity be = world.getBlockEntity(pos);
+		if (be instanceof WoodenBarrelsEntity) {
+			((WoodenBarrelsEntity)be).tick();
+		}
+	}
+	
+	@Override
+	public BlockEntity createBlockEntity(BlockView view) {
 		return new WoodenBarrelsEntity();
 	}
 }
--- src/main/java/com/brand/blockus/blocks/entity/WoodenBarrelsEntity.java
+++ src/main/java/com/brand/blockus/blocks/entity/WoodenBarrelsEntity.java
@@ -1,8 +1,8 @@
 package com.brand.blockus.blocks.entity;
 
+import com.brand.blockus.blocks.WoodenBarrels;
 import com.brand.blockus.content.Barrels;
 
-import net.minecraft.block.BarrelBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BarrelBlockEntity;
 import net.minecraft.block.entity.ChestBlockEntity;
@@ -10,11 +10,27 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3i;
 
 public class WoodenBarrelsEntity extends BarrelBlockEntity {
 	private int viewerCount;
+	
+	@Override
+	public void onInvOpen(PlayerEntity player) {
+		if (!player.isSpectator()) {
+			if (this.viewerCount < 0) {
+				this.viewerCount = 0;
+			}
+			++this.viewerCount;
+			BlockState state = this.getCachedState();
+			boolean bool = state.get(WoodenBarrels.OPEN);
+			if (!bool) {
+				this.playSound(state, SoundEvents.BLOCK_BARREL_OPEN);
+				this.setOpen(state, true);
+			}
+			this.scheduleUpdate();
+		}
+	}
 	
 	public void scheduleUpdate() {
 		this.world.getBlockTickScheduler().schedule(this.getPos(), this.getCachedState().getBlock(), 5);
@@ -34,7 +50,7 @@ public class WoodenBarrelsEntity extends BarrelBlockEntity {
 				this.invalidate();
 				return;
 			}
-			boolean boolean_1 = (Boolean)blockState_1.get(BarrelBlock.OPEN);
+			boolean boolean_1 = blockState_1.get(WoodenBarrels.OPEN);
 			if (boolean_1) {
 				this.playSound(blockState_1, SoundEvents.BLOCK_BARREL_CLOSE);
 				this.setOpen(blockState_1, false);
@@ -42,15 +58,22 @@ public class WoodenBarrelsEntity extends BarrelBlockEntity {
 		}
 	}
 	
+	@Override
+	public void onInvClose(PlayerEntity playerEntity_1) {
+		if (!playerEntity_1.isSpectator()) {
+			--this.viewerCount;
+		}
+	}
+	
 	public void setOpen(BlockState blockState_1, boolean boolean_1) {
-		this.world.setBlockState(this.getPos(), (BlockState)blockState_1.with(BarrelBlock.OPEN, boolean_1), 3);
+		this.world.setBlockState(this.getPos(), blockState_1.with(WoodenBarrels.OPEN, boolean_1), 3);
 	}
 	
 	public void playSound(BlockState blockState_1, SoundEvent soundEvent_1) {
-		Vec3i vec3i_1 = ((Direction)blockState_1.get(BarrelBlock.FACING)).getVector();
-		double double_1 = (double)this.pos.getX() + 0.5D + (double)vec3i_1.getX() / 2.0D;
-		double double_2 = (double)this.pos.getY() + 0.5D + (double)vec3i_1.getY() / 2.0D;
-		double double_3 = (double)this.pos.getZ() + 0.5D + (double)vec3i_1.getZ() / 2.0D;
-		this.world.playSound((PlayerEntity)null, double_1, double_2, double_3, soundEvent_1, SoundCategory.BLOCKS, 0.5F, this.world.random.nextFloat() * 0.1F + 0.9F);
+		Vec3i vec3i_1 = (blockState_1.get(WoodenBarrels.FACING)).getVector();
+		double double_1 = this.pos.getX() + 0.5D + vec3i_1.getX() / 2.0D;
+		double double_2 = this.pos.getY() + 0.5D + vec3i_1.getY() / 2.0D;
+		double double_3 = this.pos.getZ() + 0.5D + vec3i_1.getZ() / 2.0D;
+		this.world.playSound(null, double_1, double_2, double_3, soundEvent_1, SoundCategory.BLOCKS, 0.5F, this.world.random.nextFloat() * 0.1F + 0.9F);
 	}
 }
