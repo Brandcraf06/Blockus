6c6f8b2a7b9417bf077579dff981550f90d3ae97
Brandcraf06 <Brandcraft06@gmail.com>
2019-05-16 22:24:50 +0200

    1.1.8.1

--- src/main/java/com/brand/blockus/blocks/WoodenBarrels.java
+++ src/main/java/com/brand/blockus/blocks/WoodenBarrels.java
@@ -5,17 +5,18 @@ import com.brand.blockus.Blockus;
 
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tools.FabricToolTags;
-import net.minecraft.block.BarrelBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.Item;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.item.BlockItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class WoodenBarrels extends BarrelBlock {
+public class WoodenBarrels extends Block {
 
 	public WoodenBarrels(String name, float hardness, float resistance) {
-		super(FabricBlockSettings.of(Material.WOOD).breakByTool(FabricToolTags.AXES, 0).strength(hardness, resistance).build());
+		super(FabricBlockSettings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).breakByTool(FabricToolTags.AXES, 0).strength(hardness, resistance).build());
 		Registry.register(Registry.BLOCK, new Identifier(Blockus.MOD_ID, name), this);
 		Registry.register(Registry.ITEM,new Identifier(Blockus.MOD_ID, name), new BlockItem(this, new Item.Settings().stackSize(64).itemGroup(Blockus.BLOCKUS_DECORATIONS)));
     }
