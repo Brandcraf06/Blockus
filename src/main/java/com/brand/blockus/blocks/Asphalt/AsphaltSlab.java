443a016844eb898ad8202dba5c32323da56fb003
Brandcraf06 <Brandcraft06@gmail.com>
2020-02-07 01:08:43 +0100

    1.3.2
    Asphalt Crash Fix

--- src/main/java/com/brand/blockus/blocks/Asphalt/AsphaltSlab.java
+++ src/main/java/com/brand/blockus/blocks/Asphalt/AsphaltSlab.java
@@ -18,7 +18,25 @@ public class AsphaltSlab extends SlabBase {
 
 	@Override
 	public void onSteppedOn(World world_1, BlockPos blockPos_1, Entity entity_1) {
-		if (entity_1.getType() != EntityType.ITEM) {
+		if (entity_1.getType() != EntityType.ITEM && 
+				entity_1.getType() != EntityType.FALLING_BLOCK && 
+				entity_1.getType() != EntityType.EXPERIENCE_ORB &&
+				entity_1.getType() != EntityType.END_CRYSTAL && 
+			    entity_1.getType() != EntityType.EVOKER_FANGS && 
+			    entity_1.getType() != EntityType.LEASH_KNOT && 
+			    entity_1.getType() != EntityType.ITEM_FRAME &&
+			    entity_1.getType() != EntityType.TNT && 
+			    entity_1.getType() != EntityType.TRIDENT &&
+			    entity_1.getType() != EntityType.PAINTING &&
+				entity_1.getType() != EntityType.BOAT && 
+				entity_1.getType() != EntityType.MINECART && 
+				entity_1.getType() != EntityType.CHEST_MINECART && 
+				entity_1.getType() != EntityType.FURNACE_MINECART && 
+				entity_1.getType() != EntityType.HOPPER_MINECART && 
+				entity_1.getType() != EntityType.SPAWNER_MINECART && 
+				entity_1.getType() != EntityType.TNT_MINECART && 
+				entity_1.getType() != EntityType.COMMAND_BLOCK_MINECART && 
+				entity_1.getType() != EntityType.ARMOR_STAND) {
 			((LivingEntity) entity_1).addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 4, 4, true, false, false));
 		}
 	}
