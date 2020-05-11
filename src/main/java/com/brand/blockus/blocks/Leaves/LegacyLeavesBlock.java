e079a782d5864c39aea6ebe96c1463b1ba80072f
Brandcraf06 <Brandcraft06@gmail.com>
2020-05-11 20:13:59 +0200

    1.5.2.1 for 1.15
    Bugs fix

---
+++ src/main/java/com/brand/blockus/blocks/Leaves/LegacyLeavesBlock.java
@@ -0,0 +1,32 @@
+package com.brand.blockus.blocks.Leaves;
+
+import com.brand.blockus.Blockus;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.BlockView;
+
+public class LegacyLeavesBlock extends Block {
+
+	public static boolean translucentLeaves;
+
+	public LegacyLeavesBlock(String name, float hardness, float resistance) {
+		super(FabricBlockSettings.of(Material.LEAVES).sounds(BlockSoundGroup.GRASS).breakByTool(FabricToolTags.SHEARS, 0).strength(hardness, resistance).nonOpaque());
+		Registry.register(Registry.BLOCK, new Identifier(Blockus.MOD_ID, name), this);
+		Registry.register(Registry.ITEM,new Identifier(Blockus.MOD_ID, name), new BlockItem(this, new Item.Settings().maxCount(64).group(Blockus.BLOCKUS_BUILDING_BLOCKS)));
+
+   }
+	
+	public boolean canSuffocate(BlockState state, BlockView view, BlockPos pos) {
+		      return false;
+   }
+}
