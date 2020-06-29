e8adcec3f3a861232b1886dfc37e1a100cf106a3
Processhf <processhf@gmail.com>
2020-06-28 23:24:27 -0400

    Fix LightBlockBase Tile Drops
    Fixes LightBlockBase blocks (Lantern/Lit Redstone Lamps) not dropping tiles.
    Also matches breaking behavior of Vanilla lanterns

--- src/main/java/com/brand/blockus/blocks/Light/LightBlockBase.java
+++ src/main/java/com/brand/blockus/blocks/Light/LightBlockBase.java
@@ -17,7 +17,7 @@ import net.minecraft.util.registry.Registry;
 public class LightBlockBase extends Block {
 
 	public LightBlockBase(String name, float hardness, float resistance, Material material, BlockSoundGroup sound, ItemGroup itemgroup, int lightlevel, MaterialColor color) {
-		super(FabricBlockSettings.of(material, color).sounds(sound).lightLevel(lightlevel).requiresTool().strength(hardness, resistance));
+		super(FabricBlockSettings.of(material, color).sounds(sound).lightLevel(lightlevel).breakByTool(FabricToolTags.PICKAXES, 1).strength(hardness, resistance));
 		Registry.register(Registry.BLOCK, new Identifier(Blockus.MOD_ID, name), this);
 		Registry.register(Registry.ITEM,new Identifier(Blockus.MOD_ID, name), new BlockItem(this, new Item.Settings().maxCount(64).group(itemgroup)));
 	}
