bb7807626a9ea7fc6af9c1394641b992bb181f68
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-07-19 02:10:34 -0400

    Add rough red sandstone

--- src/main/java/com/brand/blockus/content/SandstoneBricks.java
+++ src/main/java/com/brand/blockus/content/SandstoneBricks.java
@@ -26,6 +26,7 @@ public class SandstoneBricks {
 	public static final SlabBase SMALL_SANDSTONE_BRICKS_SLAB = new SlabBase("small_sandstone_bricks_slab", SMALL_SANDSTONE_BRICKS);
 	
 	// Red Sandstone	
+	public static final BlockBase ROUGH_RED_SANDSTONE = new BlockBase("rough_red_sandstone", 0.8f, 0.8f, Material.STONE, BlockSoundGroup.STONE, Blockus.BLOCKUS_BUILDING_BLOCKS, MaterialColor.SAND);
 	public static final BlockBase RED_SANDSTONE_BRICKS = new BlockBase("red_sandstone_bricks", 0.8f, 0.8f, Material.STONE, BlockSoundGroup.STONE, Blockus.BLOCKUS_BUILDING_BLOCKS, MaterialColor.SAND);
 	public static final StairsBase RED_SANDSTONE_BRICKS_STAIRS = new StairsBase(RED_SANDSTONE_BRICKS.getDefaultState(), "red_sandstone_bricks_stairs", RED_SANDSTONE_BRICKS);
 	public static final SlabBase RED_SANDSTONE_BRICKS_SLAB = new SlabBase("red_sandstone_bricks_slab", RED_SANDSTONE_BRICKS);
