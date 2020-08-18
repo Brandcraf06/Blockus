010c5d2d9dd523ba4620735d5c9743163f27fc31
shedaniel <daniel@shedaniel.me>
2020-08-18 19:23:57 +0800

    1.16.2 Port

--- src/main/java/com/brand/blockus/blocks/Wood/WoodButtonBase.java
+++ src/main/java/com/brand/blockus/blocks/Wood/WoodButtonBase.java
@@ -5,14 +5,14 @@ import com.brand.blockus.Blockus;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Material;
-import net.minecraft.block.WoodButtonBlock;
+import net.minecraft.block.WoodenButtonBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class WoodButtonBase extends WoodButtonBlock {
+public class WoodButtonBase extends WoodenButtonBlock {
 	
 	public WoodButtonBase(String name, float hardness, float resistance) {
 		super(FabricBlockSettings.of(Material.SUPPORTED).sounds(BlockSoundGroup.WOOD).breakByTool(FabricToolTags.AXES, 0).noCollision().strength(hardness, resistance));
