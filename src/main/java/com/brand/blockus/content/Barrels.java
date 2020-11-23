256924e6bc0161639dfd41e782b44f090e181cdd
Brandcraft06 <Brandcraft06@gmail.com>
2020-11-23 01:27:16 +0100

    Update Barrels.java

--- src/main/java/com/brand/blockus/content/Barrels.java
+++ src/main/java/com/brand/blockus/content/Barrels.java
@@ -3,7 +3,6 @@ package com.brand.blockus.content;
 import com.brand.blockus.Blockus;
 import com.brand.blockus.blockentity.WoodenBarrelBlockEntity;
 import com.brand.blockus.blocks.Wood.WoodenBarrels;
-import com.mojang.datafixers.types.Type;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
@@ -25,10 +24,8 @@ public class Barrels {
 
     public static final BlockEntityType<WoodenBarrelBlockEntity> WOODEN_BARREL = registerBe("wooden_barrel", BlockEntityType.Builder.create(WoodenBarrelBlockEntity::new, OAK_BARREL, BIRCH_BARREL, JUNGLE_BARREL, ACACIA_BARREL, CRIMSON_BARREL, WARPED_BARREL, DARK_OAK_BARREL, WHITE_OAK_BARREL, BAMBOO_BARREL, CHARRED_BARREL));
 
-    @SuppressWarnings("rawtypes")
     private static <T extends BlockEntity> BlockEntityType<T> registerBe(String name, BlockEntityType.Builder<T> builder) {
-        Type type = null;
-        return Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier(Blockus.MOD_ID, name), builder.build(type));
+        return Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier(Blockus.MOD_ID, name), builder.build(null));
     }
 
 }
