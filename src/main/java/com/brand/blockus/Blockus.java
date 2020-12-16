5277a215b45e29aa90357549a28322075fdbd79d
Brandcraft06 <Brandcraft06@gmail.com>
2020-12-16 23:13:12 +0100

    20w51a + WorldGen

--- src/main/java/com/brand/blockus/Blockus.java
+++ src/main/java/com/brand/blockus/Blockus.java
@@ -3,16 +3,10 @@ package com.brand.blockus;
 import com.brand.blockus.content.BlockusBlocks;
 import com.brand.blockus.world.BlockusConfiguredFeatures;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BiomeKeys;
-import net.minecraft.world.gen.GenerationStep;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -29,21 +23,8 @@ public class Blockus implements ModInitializer {
 
         new BlockusBlocks();
         Instance.init();
-
-        BiomeModifications.create(new Identifier(MOD_ID, "world_features"))
-                .add(ModificationPhase.ADDITIONS,
-                        BiomeSelectors.foundInOverworld(),
-                        context -> {
-                            context.getGenerationSettings().addBuiltInFeature(GenerationStep.Feature.UNDERGROUND_ORES, BlockusConfiguredFeatures.LIMESTONE);
-                            context.getGenerationSettings().addBuiltInFeature(GenerationStep.Feature.UNDERGROUND_ORES, BlockusConfiguredFeatures.BLUESTONE);
-                            context.getGenerationSettings().addBuiltInFeature(GenerationStep.Feature.UNDERGROUND_ORES, BlockusConfiguredFeatures.MARBLE);
-                        })
-                .add(ModificationPhase.ADDITIONS,
-                        BiomeSelectors.foundInOverworld().and(BiomeSelectors.includeByKey(BiomeKeys.FOREST, BiomeKeys.WOODED_HILLS, BiomeKeys.FLOWER_FOREST)),
-                        context -> context.getGenerationSettings().addBuiltInFeature(GenerationStep.Feature.VEGETAL_DECORATION, BlockusConfiguredFeatures.WHITE_OAK_TREE))
-                .add(ModificationPhase.ADDITIONS,
-                        BiomeSelectors.foundInOverworld().and(BiomeSelectors.categories(Biome.Category.PLAINS)),
-                        context -> context.getGenerationSettings().addBuiltInFeature(GenerationStep.Feature.VEGETAL_DECORATION, BlockusConfiguredFeatures.WHITE_OAK_TREE_RARE));
+        BlockusConfiguredFeatures.registerConfiguredFeature();
+        BlockusConfiguredFeatures.registerBiomeModifications();
     }
 }
 
--- src/main/java/com/brand/blockus/world/BlockusConfiguredFeatures.java
+++ src/main/java/com/brand/blockus/world/BlockusConfiguredFeatures.java
@@ -2,12 +2,15 @@ package com.brand.blockus.world;
 
 import com.brand.blockus.Blockus;
 import com.brand.blockus.content.BlockusBlocks;
-import net.minecraft.block.BlockState;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.UniformIntDistribution;
 import net.minecraft.world.gen.decorator.CountExtraDecoratorConfig;
 import net.minecraft.world.gen.decorator.Decorator;
@@ -18,8 +21,6 @@ import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
 import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider;
 import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
 
-import java.util.Map;
-
 public class BlockusConfiguredFeatures {
 
     public static final TreeFeatureConfig WHITE_OAK_TREE_CONFIG;
@@ -28,22 +29,14 @@ public class BlockusConfiguredFeatures {
     public static final ConfiguredFeature<?, ?> MARBLE;
     public static final ConfiguredFeature<?, ?> WHITE_OAK_TREE;
     public static final ConfiguredFeature<?, ?> WHITE_OAK_TREE_RARE;
-    private static final BlockState WHITE_OAK_LOG;
-    private static final BlockState WHITE_OAK_LEAVES;
 
-    // Will run when registerAndAddConfiguredFeatures is called
     static {
-        WHITE_OAK_LOG = BlockusBlocks.WHITE_OAK_LOG.getDefaultState();
-        WHITE_OAK_LEAVES = BlockusBlocks.WHITE_OAK_LEAVES.getDefaultState();
-        WHITE_OAK_TREE_CONFIG = (new TreeFeatureConfig.Builder(new SimpleBlockStateProvider(WHITE_OAK_LOG), new SimpleBlockStateProvider(WHITE_OAK_LEAVES), new BlobFoliagePlacer(UniformIntDistribution.of(2), UniformIntDistribution.of(0), 5), new StraightTrunkPlacer(7, 2, 0), new TwoLayersFeatureSize(1, 0, 1))).ignoreVines().build();
-
         LIMESTONE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockusBlocks.LIMESTONE.getDefaultState(), 33)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(25, 0, 120)).spreadHorizontally().repeat(8));
         BLUESTONE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockusBlocks.BLUESTONE.getDefaultState(), 30)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0, 0, 25)).spreadHorizontally().repeat(6));
         MARBLE = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockusBlocks.MARBLE.getDefaultState(), 40)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0, 0, 75)).spreadHorizontally().repeat(1));
+        WHITE_OAK_TREE_CONFIG = (new TreeFeatureConfig.Builder(new SimpleBlockStateProvider(BlockusBlocks.WHITE_OAK_LOG.getDefaultState()), new SimpleBlockStateProvider(BlockusBlocks.WHITE_OAK_LEAVES.getDefaultState()), new BlobFoliagePlacer(UniformIntDistribution.of(2), UniformIntDistribution.of(0), 5), new StraightTrunkPlacer(7, 2, 0), new TwoLayersFeatureSize(1, 0, 1))).ignoreVines().build();
         WHITE_OAK_TREE = Feature.TREE.configure(WHITE_OAK_TREE_CONFIG).decorate(ConfiguredFeatures.Decorators.SQUARE_HEIGHTMAP).decorate(Decorator.COUNT_EXTRA.configure(new CountExtraDecoratorConfig(0, 0.05F, 1)));
         WHITE_OAK_TREE_RARE = Feature.TREE.configure(WHITE_OAK_TREE_CONFIG).decorate(ConfiguredFeatures.Decorators.SQUARE_HEIGHTMAP).decorate(Decorator.COUNT_EXTRA.configure(new CountExtraDecoratorConfig(0, 0.0085F, 1)));
-
-        registerConfiguredFeature();
     }
 
     public static void registerConfiguredFeature() {
@@ -54,29 +47,20 @@ public class BlockusConfiguredFeatures {
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Blockus.MOD_ID, "white_oak_tree_rare"), WHITE_OAK_TREE_RARE);
     }
 
-    // Based on Applied-Energistics-2's way of registering and adding CFs.
-    //
-    // We are making an Instance of a ConfiguredFeature here in this class,
-    // registering it to BuiltInRegisteries and adding it to the BuiltInRegisteries biomes.
-    //
-    // Now when we click on a world to enter, the DynamicRegisteries fires, copies the BuiltInRegisteries
-    // entries and **makes its own instances** of everything in there. A copy of the object but not the
-    // same object. Since we registered to the BuiltInRegisteries and added the CFs to BuiltInRegisteries
-    // biomes, the CF Instance in the DynamicRegistries' CF registry and in the DynamicRegistries' biomes
-    // will now match and things work smoothly.
-    //
-    // If we registered the CF to the BuiltInRegisteries CF registry BUT add this CF to the DynamicRegistries's
-    // biomes by DynamicRegistryCallback.callback, the CF Instance the DynamicRegistry will make for it's
-    // CF registry will not be the same object as the CF in the biome. THIS is the key point that causes so much
-    // confusion. If we want to add to the biomes in the DynamicRegistries directly, we need to add our CF to the
-    // DynamicRegistries's CF registry directly instead of the BuiltInRegistries. Basically, you can't mix and match
-    // whether to register/add to BuiltIn or Dynamic. You have to either fully use BuiltIn or fully use Dynamic to
-    // make life easier.
-    public static void registerAndAddConfiguredFeatures() {
-        for (Map.Entry<RegistryKey<Biome>, Biome> entry : BuiltinRegistries.BIOME.getEntries()) {
-            BlockusGen.addMineables(entry.getValue());
-            BlockusGen.addWhiteOakTrees(entry.getKey(), entry.getValue());
-            BlockusGen.addPlainsWhiteOakTrees(entry.getValue());
-        }
+    public static void registerBiomeModifications() {
+        BiomeModifications.create(new Identifier(Blockus.MOD_ID, "world_features"))
+                .add(ModificationPhase.ADDITIONS,
+                        BiomeSelectors.foundInOverworld(),
+                        context -> {
+                            context.getGenerationSettings().addBuiltInFeature(GenerationStep.Feature.UNDERGROUND_ORES, LIMESTONE);
+                            context.getGenerationSettings().addBuiltInFeature(GenerationStep.Feature.UNDERGROUND_ORES, BLUESTONE);
+                            context.getGenerationSettings().addBuiltInFeature(GenerationStep.Feature.UNDERGROUND_ORES, MARBLE);
+                        })
+                .add(ModificationPhase.ADDITIONS,
+                        BiomeSelectors.foundInOverworld().and(BiomeSelectors.includeByKey(BiomeKeys.FOREST, BiomeKeys.WOODED_HILLS, BiomeKeys.FLOWER_FOREST)),
+                        context -> context.getGenerationSettings().addBuiltInFeature(GenerationStep.Feature.VEGETAL_DECORATION, WHITE_OAK_TREE))
+                .add(ModificationPhase.ADDITIONS,
+                        BiomeSelectors.foundInOverworld().and(BiomeSelectors.categories(Biome.Category.PLAINS)),
+                        context -> context.getGenerationSettings().addBuiltInFeature(GenerationStep.Feature.VEGETAL_DECORATION, WHITE_OAK_TREE_RARE));
     }
 }
