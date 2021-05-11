a6dcc52489a2914e819544f4d984820add9df87f
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-05-11 19:27:58 -0400

    Add water brick columns

--- src/main/java/com/brand/blockus/content/BlockusColumnBlocks.java
+++ src/main/java/com/brand/blockus/content/BlockusColumnBlocks.java
@@ -26,6 +26,7 @@ public final class BlockusColumnBlocks {
     }
 
     public static void init() {
+        registerColumnBlockAndItem("water_brick_column", BlockusBlocks.WATER_BRICKS);
         registerColumnBlockAndItem("blaze_brick_column", BlockusBlocks.BLAZE_BRICKS);
     }
 }
