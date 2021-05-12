5bdf1b7f22486a8134e8dfcde07ef18c49d6da23
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-05-11 22:31:25 -0400

    Add soul sandstone columns

--- src/main/java/com/brand/blockus/content/BlockusColumnBlocks.java
+++ src/main/java/com/brand/blockus/content/BlockusColumnBlocks.java
@@ -56,6 +56,7 @@ public final class BlockusColumnBlocks {
         registerColumnBlockAndItem("small_sandstone_brick_column", BlockusBlocks.SMALL_SANDSTONE_BRICKS);
         registerColumnBlockAndItem("red_sandstone_brick_column", BlockusBlocks.RED_SANDSTONE_BRICKS);
         registerColumnBlockAndItem("small_red_sandstone_brick_column", BlockusBlocks.SMALL_RED_SANDSTONE_BRICKS);
+        registerColumnBlockAndItem("soul_sandstone_column", BlockusBlocks.SOUL_SANDSTONE);
         registerColumnBlockAndItem("soul_sandstone_brick_column", BlockusBlocks.SOUL_SANDSTONE_BRICKS);
         registerColumnBlockAndItem("honeycomb_brick_column", BlockusBlocks.HONEYCOMB_BRICKS);
         registerColumnBlockAndItem("purpur_brick_column", BlockusBlocks.PURPUR_BRICKS);
