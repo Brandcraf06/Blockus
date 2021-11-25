bcae09096ef5c1df4257a112e58ec02a63b2edda
Brandcraft06 <Brandcraft06@gmail.com>
2021-11-25 20:06:41 +0100

    Update BlockusOreFeatures.java

--- src/main/java/com/brand/blockus/world/BlockusOreFeatures.java
+++ src/main/java/com/brand/blockus/world/BlockusOreFeatures.java
@@ -5,13 +5,8 @@ import net.minecraft.world.gen.feature.*;
 
 public class BlockusOreFeatures {
 
-    public static final ConfiguredFeature<?, ?> ORE_LIMESTONE;
-    public static final ConfiguredFeature<?, ?> ORE_MARBLE;
-    public static final ConfiguredFeature<?, ?> ORE_BLUESTONE;
+    public static final ConfiguredFeature<?, ?> ORE_LIMESTONE = ConfiguredFeatures.register("ore_limestone", Feature.ORE.configure(new OreFeatureConfig(OreConfiguredFeatures.BASE_STONE_OVERWORLD, BlockusBlocks.LIMESTONE.getDefaultState(), 64)));
+    public static final ConfiguredFeature<?, ?> ORE_MARBLE = ConfiguredFeatures.register("ore_marble", Feature.ORE.configure(new OreFeatureConfig(OreConfiguredFeatures.BASE_STONE_OVERWORLD, BlockusBlocks.MARBLE.getDefaultState(), 64)));
+    public static final ConfiguredFeature<?, ?> ORE_BLUESTONE = ConfiguredFeatures.register("ore_bluestone", Feature.ORE.configure(new OreFeatureConfig(OreConfiguredFeatures.BASE_STONE_OVERWORLD, BlockusBlocks.BLUESTONE.getDefaultState(), 64)));
 
-    static {
-        ORE_LIMESTONE = ConfiguredFeatures.register("ore_limestone", Feature.ORE.configure(new OreFeatureConfig(OreConfiguredFeatures.BASE_STONE_OVERWORLD, BlockusBlocks.LIMESTONE.getDefaultState(), 64)));
-        ORE_MARBLE = ConfiguredFeatures.register("ore_marble", Feature.ORE.configure(new OreFeatureConfig(OreConfiguredFeatures.BASE_STONE_OVERWORLD, BlockusBlocks.MARBLE.getDefaultState(), 64)));
-        ORE_BLUESTONE = ConfiguredFeatures.register("ore_bluestone", Feature.ORE.configure(new OreFeatureConfig(OreConfiguredFeatures.BASE_STONE_OVERWORLD, BlockusBlocks.BLUESTONE.getDefaultState(), 64)));
-    }
 }
