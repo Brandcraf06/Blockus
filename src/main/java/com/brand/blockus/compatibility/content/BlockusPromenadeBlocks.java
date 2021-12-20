7e4e16d3040bb6d6dfbb83ab4877a0c07e271b2f
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-12-19 20:47:30 -0500

    Allow putting pink cherry oak trees in large flower pots

--- src/main/java/com/brand/blockus/compatibility/content/BlockusPromenadeBlocks.java
+++ src/main/java/com/brand/blockus/compatibility/content/BlockusPromenadeBlocks.java
@@ -2,6 +2,7 @@ package com.brand.blockus.compatibility.content;
 
 import com.brand.blockus.content.BlocksRegistration;
 import com.hugman.promenade.init.AutumnBundle;
+import com.hugman.promenade.init.CherryBundle;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -13,20 +14,24 @@ import net.minecraft.client.render.RenderLayer;
 public class BlockusPromenadeBlocks {
     private static Block POTTED_AUTUMN_OAK;
     private static Block POTTED_AUTUMN_BIRCH;
+    private static Block POTTED_PINK_CHERRY_OAK;
 
     public static void init() {
         POTTED_AUTUMN_OAK = BlocksRegistration.registerPottedDoublePlant(AutumnBundle.AUTUMN_OAK_SAPLING.getPlant(), "autumn_oak");
         POTTED_AUTUMN_BIRCH = BlocksRegistration.registerPottedDoublePlant(AutumnBundle.AUTUMN_BIRCH_SAPLING.getPlant(), "autumn_birch");
+        POTTED_PINK_CHERRY_OAK = BlocksRegistration.registerPottedDoublePlant(CherryBundle.PINK_CHERRY_OAK_SAPLING.getPlant(), "pink_cherry_oak");
     }
 
     @Environment(EnvType.CLIENT)
     public static void initClient() {
         ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> 0xE86D30, POTTED_AUTUMN_OAK);
         ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> 0xFFBA42, POTTED_AUTUMN_BIRCH);
+        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> 0xF09AC3, POTTED_PINK_CHERRY_OAK);
 
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutoutMipped(),
                 POTTED_AUTUMN_OAK,
-                POTTED_AUTUMN_BIRCH
+                POTTED_AUTUMN_BIRCH,
+                POTTED_PINK_CHERRY_OAK
         );
     }
 }
