b2c8f70eb75376cbe91a1615db95a98849ad069f
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-12-19 20:41:59 -0500

    Allow putting autumn birch trees in large flower pots

--- src/main/java/com/brand/blockus/compatibility/content/BlockusPromenadeBlocks.java
+++ src/main/java/com/brand/blockus/compatibility/content/BlockusPromenadeBlocks.java
@@ -12,17 +12,21 @@ import net.minecraft.client.render.RenderLayer;
 
 public class BlockusPromenadeBlocks {
     private static Block POTTED_AUTUMN_OAK;
+    private static Block POTTED_AUTUMN_BIRCH;
 
     public static void init() {
         POTTED_AUTUMN_OAK = BlocksRegistration.registerPottedDoublePlant(AutumnBundle.AUTUMN_OAK_SAPLING.getPlant(), "autumn_oak");
+        POTTED_AUTUMN_BIRCH = BlocksRegistration.registerPottedDoublePlant(AutumnBundle.AUTUMN_BIRCH_SAPLING.getPlant(), "autumn_birch");
     }
 
     @Environment(EnvType.CLIENT)
     public static void initClient() {
         ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> 0xE86D30, POTTED_AUTUMN_OAK);
+        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> 0xFFBA42, POTTED_AUTUMN_BIRCH);
 
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutoutMipped(),
-                POTTED_AUTUMN_OAK
+                POTTED_AUTUMN_OAK,
+                POTTED_AUTUMN_BIRCH
         );
     }
 }
