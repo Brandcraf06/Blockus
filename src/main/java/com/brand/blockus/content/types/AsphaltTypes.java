bfc58523e62fa547ee5e1185c6d6a9ba375f1463
Brandcraft06 <Brandcraft06@gmail.com>
2022-03-09 00:11:19 +0100

    Update AsphaltTypes.java

--- src/main/java/com/brand/blockus/content/types/AsphaltTypes.java
+++ src/main/java/com/brand/blockus/content/types/AsphaltTypes.java
@@ -18,22 +18,22 @@ import net.minecraft.util.registry.Registry;
 
 public enum AsphaltTypes {
 
-    ASPHALT("asphalt", DyeColor.BLACK),
-    WHITE_ASPHALT("white_asphalt", DyeColor.WHITE),
-    ORANGE_ASPHALT("orange_asphalt", DyeColor.ORANGE),
-    MAGENTA_ASPHALT("magenta_asphalt", DyeColor.MAGENTA),
-    LIGHT_BLUE_ASPHALT("light_blue_asphalt", DyeColor.LIGHT_BLUE),
-    YELLOW_ASPHALT("yellow_asphalt", DyeColor.YELLOW),
-    LIME_ASPHALT("lime_asphalt", DyeColor.LIME),
-    PINK_ASPHALT("pink_asphalt", DyeColor.PINK),
-    LIGHT_GRAY_ASPHALT("light_gray_asphalt", DyeColor.LIGHT_GRAY),
-    GRAY_ASPHALT("gray_asphalt", DyeColor.GRAY),
-    CYAN_ASPHALT("cyan_asphalt", DyeColor.CYAN),
-    PURPLE_ASPHALT("purple_asphalt", DyeColor.PURPLE),
-    BLUE_ASPHALT("blue_asphalt", DyeColor.BLUE),
-    BROWN_ASPHALT("brown_asphalt", DyeColor.BROWN),
-    GREEN_ASPHALT("green_asphalt", DyeColor.GREEN),
-    RED_ASPHALT("red_asphalt", DyeColor.RED);
+    ASPHALT(DyeColor.BLACK),
+    WHITE_ASPHALT(DyeColor.WHITE),
+    ORANGE_ASPHALT(DyeColor.ORANGE),
+    MAGENTA_ASPHALT(DyeColor.MAGENTA),
+    LIGHT_BLUE_ASPHALT(DyeColor.LIGHT_BLUE),
+    YELLOW_ASPHALT(DyeColor.YELLOW),
+    LIME_ASPHALT(DyeColor.LIME),
+    PINK_ASPHALT(DyeColor.PINK),
+    LIGHT_GRAY_ASPHALT(DyeColor.LIGHT_GRAY),
+    GRAY_ASPHALT(DyeColor.GRAY),
+    CYAN_ASPHALT(DyeColor.CYAN),
+    PURPLE_ASPHALT(DyeColor.PURPLE),
+    BLUE_ASPHALT(DyeColor.BLUE),
+    BROWN_ASPHALT(DyeColor.BROWN),
+    GREEN_ASPHALT(DyeColor.GREEN),
+    RED_ASPHALT(DyeColor.RED);
 
     public final AsphaltBlock block;
     public final AsphaltSlab slab;
@@ -42,8 +42,8 @@ public enum AsphaltTypes {
     public final SpeedBlockItem item_slab;
     public final SpeedBlockItem item_stairs;
 
-    private AsphaltTypes(String type, DyeColor color) {
-        type = color.getName() + "_asphalt";
+    private AsphaltTypes(DyeColor color) {
+        String type = color.getName() + "_asphalt";
         String type2 = type.replace("black_asphalt", "asphalt");
         Identifier id = Blockus.id(type2);
         Identifier id_slab = Blockus.id(type2 + "_slab");
