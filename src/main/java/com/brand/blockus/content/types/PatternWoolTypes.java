fabc6c0f094dba3fc493dba530caf3958ef58a4f
Brandcraft06 <Brandcraft06@gmail.com>
2023-02-22 23:20:53 +0100

    bump version

--- src/main/java/com/brand/blockus/content/types/PatternWoolTypes.java
+++ src/main/java/com/brand/blockus/content/types/PatternWoolTypes.java
@@ -1,9 +1,5 @@
 package com.brand.blockus.content.types;
 
-import com.brand.blockus.blocks.base.StairsBase;
-import com.brand.blockus.blocks.base.asphalt.AsphaltBlock;
-import com.brand.blockus.blocks.base.asphalt.AsphaltSlab;
-import com.brand.blockus.blocks.base.asphalt.AsphaltStairs;
 import com.brand.blockus.content.BlocksRegistration;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
@@ -26,8 +22,8 @@ public class PatternWoolTypes {
         Block.Settings blockSettings = FabricBlockSettings.of(Material.WOOL, color).strength(0.8F).sounds(BlockSoundGroup.WOOL);
 
         this.block = BlocksRegistration.register(type, new Block(blockSettings));
-        this.slab = BlocksRegistration.register(type + "_slab", new SlabBlock(FabricBlockSettings.copyOf(block)));
-        this.stairs = BlocksRegistration.register(type + "_stairs", new StairsBase(block.getDefaultState(), FabricBlockSettings.copyOf(block)));
+        this.slab = BlocksRegistration.registerSlab(this.block);
+        this.stairs = BlocksRegistration.registerStairs(this.block);
         this.carpet = BlocksRegistration.register(type.replace("wool", "carpet"), new DyedCarpetBlock(dyecolor, FabricBlockSettings.of(Material.CARPET, color).strength(0.1F).sounds(BlockSoundGroup.WOOL)));
 
 
