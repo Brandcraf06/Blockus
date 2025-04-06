9299817b0867fcf1ad724b21b2e8c3be697a5c85
Brandcraft06 <Brandcraft06@gmail.com>
2025-04-06 16:15:36 +0200

    Update BlockusAliases.java

--- src/main/java/com/brand/blockus/registry/BlockusAliases.java
+++ src/main/java/com/brand/blockus/registry/BlockusAliases.java
@@ -6,5 +6,6 @@ import net.minecraft.registry.Registries;
 public class BlockusAliases {
     public static void init() {
             Registries.BLOCK.addAlias(Blockus.id("rainbow_petal"), Blockus.id("rainbow_petals"));
+            Registries.ITEM.addAlias(Blockus.id("rainbow_petal"), Blockus.id("rainbow_petals"));
     }
 }
