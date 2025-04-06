05231cab96614e2b9fbc630c174963d8ead578d6
Brandcraft06 <Brandcraft06@gmail.com>
2025-04-06 16:14:26 +0200

    Fix aliases problem

--- src/main/java/com/brand/blockus/registry/BlockusAliases.java
+++ src/main/java/com/brand/blockus/registry/BlockusAliases.java
@@ -3,7 +3,7 @@ package com.brand.blockus.registry;
 import com.brand.blockus.Blockus;
 import net.minecraft.registry.Registries;
 
-public class RegistryAliases {
+public class BlockusAliases {
     public static void init() {
             Registries.BLOCK.addAlias(Blockus.id("rainbow_petal"), Blockus.id("rainbow_petals"));
     }
