31551f166bf6cbb46d67897289a21fba8203cf04
Brandcraft06 <Brandcraft06@gmail.com>
2025-06-27 18:00:18 +0200

    Update NetherPortalMixin.java

--- src/main/java/com/brand/blockus/mixin/NetherPortalMixin.java
+++ src/main/java/com/brand/blockus/mixin/NetherPortalMixin.java
@@ -1,35 +1,27 @@
 package com.brand.blockus.mixin;
 
 import com.brand.blockus.registry.tag.BlockusBlockTags;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.dimension.NetherPortal;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(NetherPortal.class)
 public class NetherPortalMixin {
 
-    private static boolean validFrameBlock(AbstractBlock.ContextPredicate predicate, BlockState state, BlockView world, BlockPos pos, Operation<Boolean> original) {
-        return original.call(predicate, state, world, pos) || state.isIn(BlockusBlockTags.NETHER_PORTAL_FRAME_BLOCKS);
-    }
+    @Shadow @Mutable
+    private static AbstractBlock.ContextPredicate IS_VALID_FRAME_BLOCK;
 
-    @WrapOperation(method = "getWidth", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/AbstractBlock$ContextPredicate;test(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Z"))
-    private static boolean getWidth(AbstractBlock.ContextPredicate predicate, BlockState state, BlockView world, BlockPos pos, Operation<Boolean> original) {
-        return validFrameBlock(predicate, state, world, pos, original);
-    }
-
-    @WrapOperation(method = "getPotentialHeight", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/AbstractBlock$ContextPredicate;test(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Z"))
-    private static boolean getPotentialHeight(AbstractBlock.ContextPredicate predicate, BlockState state, BlockView world, BlockPos pos, Operation<Boolean> original) {
-        return validFrameBlock(predicate, state, world, pos, original);
-    }
-
-    @WrapOperation(method = "isHorizontalFrameValid", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/AbstractBlock$ContextPredicate;test(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Z"))
-    private static boolean isHorizontalFrameValid(AbstractBlock.ContextPredicate predicate, BlockState state, BlockView world, BlockPos pos, Operation<Boolean> original) {
-        return validFrameBlock(predicate, state, world, pos, original);
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void validFrameBlock(CallbackInfo ci) {
+        AbstractBlock.ContextPredicate original = IS_VALID_FRAME_BLOCK;
+        IS_VALID_FRAME_BLOCK = (BlockState state, BlockView world, BlockPos pos) -> original.test(state, world, pos) || state.isIn(BlockusBlockTags.NETHER_PORTAL_FRAME_BLOCKS);
     }
 }
